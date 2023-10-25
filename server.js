808551e1a4dd6b12d6cd820bc913657c22b54805
IzaanArain <izaansaquib@gmail.com>
2023-10-25 08:12:00 +0500

    updated

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const dotenv = require("dotenv");
 dotenv.config();
 const Connect = require("./config/ConnectionDB");
 const colors = require("colors");
-const userRoutes=require("./routes/UserRoutes")
+const userRoutes=require("./routes/UserRoutes");
+const { requstDetails } = require("./middleware/RequestDetails");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(requstDetails)
 app.use("/api/v1/user",userRoutes)
 app.use("/uploads",express.static("uploads"));
 
